a1ade4dbc0997e48f1cef35782491270ad803481
SWLim <seckwei93@gmail.com>
2016-03-19 19:07:30 +0000

    Serving client code

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ var express = require('express'),
 
 var SERVER_PORT = process.env.PORT || 8000;
 
-app.get('/', function (req, res) {
-    res.end('Test');
-});
+app.use(express.static('client'));
 
 app.listen(SERVER_PORT, function(){
     console.log('Listening on 8000');
