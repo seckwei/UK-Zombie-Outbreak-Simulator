9173c859383dc81c619d482f5648bbe4cacd4182
Sam Welek <s.welek@outlook.com>
2016-03-20 11:10:59 +0000

    It does some things

--- sql/spInfection.sql
+++ sql/spInfection.sql
@@ -16,13 +16,10 @@ CREATE PROCEDURE spInfection(
 BEGIN
 	DECLARE CWeekNum int;
     DECLARE SpreadCount int;
-    
+    DECLARE CMultiplier numeric(18,9);
     
     CREATE TABLE tmpResults (	WeekNum int,
 											DistrictID int,
-											DistrictName nvarchar(100),
-                                            Longitude decimal(18,9),
-                                            Latitude decimal (18,9),
                                             TotalPopulation int,
 											TotalInfected int,
 											Speed numeric(18,9),
@@ -36,9 +33,6 @@ BEGIN
 		IF (CWeekNum = 0) THEN
 			INSERT INTO tmpResults (WeekNum, 
 										DistrictID,
-										DistrictName, 
-                                        Longitude,
-                                        Latitude,
                                         TotalPopulation,
 										TotalInfected,
 										Speed,
@@ -46,9 +40,6 @@ BEGIN
                                         Spread) 
 				SELECT 	0, 
 						D.ID,
-						D.Name,
-						D.Longitude,
-						D.Latitude,
                         E.Employed+E.Unemployed,
 						200,
                         D.StartSpeed,
@@ -57,24 +48,19 @@ BEGIN
 				FROM 	District D
 						LEFT OUTER JOIN Employment E on E.DistrictID = D.ID
 				WHERE	Name = dName;
-		ELSE IF (CWeekNum < 26) THEN
-         SET SpreadCount = SpreadCount +(SELECT COUNT(Spread) FROM tmpResults WHERE Spread = 1);
-			IF EXISTS (SELECT * FROM tmpResults WHere Spread = 1 AND WeekNum = (SELECT MAX(WeekNum))) THEN
+		ELSEIF (CWeekNum < 26) THEN
+			SET SpreadCount = CASE (SELECT COUNT(Spread) FROM tmpResults WHERE Spread = 1) WHEN 0 THEN 1 ELSE (SELECT COUNT(Spread) FROM tmpResults WHERE Spread = 1) END;
+			IF EXISTS (SELECT * FROM tmpResults WHERE Spread = 1 AND WeekNum = CWeekNum - 1) THEN
+            
 				INSERT INTO tmpResults (WeekNum, 
 										DistrictID,
-										DistrictName, 
-                                        Longitude,
-                                        Latitude,
 									    TotalPopulation,
 									    TotalInfected,
 										Speed,
                                         Multiplier,
                                         Spread) 
-					SELECT		DISTINCT CWeekNum,
+					SELECT	    CWeekNum,
 								C.ToID,
-								D.Name,
-                                D.Longitude,
-                                D.Latitude,
 								E.Employed+E.Unemployed,
 								20,
 								D.StartSpeed,
@@ -82,15 +68,14 @@ BEGIN
 								0
 					FROM 		Commute C
                                 LEFT OUTER JOIN District D ON C.ToID = D.ID
-								LEFT OUTER JOIN Employment E on E.DistrictID = D.ID   
+								LEFT OUTER JOIN Employment E on E.DistrictID = D.ID
+					WHERE		NOT EXISTS (SELECT * FROM tmpResults WHERE DistrictID = D.ID)
 					ORDER BY 	C.Number DESC
 					LIMIT 		5;
+				
 			END IF;
 			INSERT INTO tmpResults (WeekNum, 
 										DistrictID,
-										DistrictName, 
-                                        Longitude,
-                                        Latitude, 
                                         TotalPopulation,
 									    TotalInfected,
 										Speed,
@@ -98,91 +83,97 @@ BEGIN
                                         Spread) 
 				SELECT 	CWeekNum, 
 						t.DistrictID,
-						t.DistrictName, 
-                        t.Longitude,
-                        t.Latitude, 
                         t.TotalPopulation,
-						t.TotalInfected * (t.Multiplier + 0.185),
-                        CEILING(D.StartSpeed * (t.Multiplier + 0.185)),
+						t.TotalInfected * ((t.Multiplier * 0.989) + 0.185),
+                        CEILING(D.StartSpeed * ((t.Multiplier * 0.989) + 0.185)),
                         (t.Multiplier * 0.989),
-                        CASE 	WHEN (t.TotalPopulation/2) <= t.TotalInfected 
-								THEN 1 
-                                ELSE 0 
+                        CASE ((t.TotalPopulation/4) <= t.TotalInfected) 
+								WHEN TRUE
+									THEN 1 
+								ELSE 0 
 						END
 				FROM 	District D
 						LEFT OUTER JOIN tmpResults t ON t.DistrictID = D.ID
 				WHERE	t.WeekNum = (SELECT MAX(WeekNum))
-                   ORDER BY	WeekNum DESC
-                limit SpreadCount;
+                AND		(
+							SELECT 	t2.DistrictID 
+                            FROM 	tmpResults t2 
+                            WHERE 	t2.DistrictID = D.ID 
+                            AND 	t2.WeekNum = CWeekNum
+                            LIMIT	1
+						) IS NULL
+				ORDER BY	WeekNum DESC
+				LIMIT 	SpreadCount;
                
 		ELSE 
-        SET SpreadCount = SpreadCount +(SELECT COUNT(Spread) FROM tmpResults WHERE Spread = 1);
-			IF EXISTS (SELECT * FROM tmpResults WHere Spread = 1 AND WeekNum = (SELECT MAX(WeekNum))) THEN
+			SET SpreadCount = CASE (SELECT COUNT(Spread) FROM tmpResults WHERE Spread = 1) WHEN 0 THEN 1 ELSE (SELECT COUNT(Spread) FROM tmpResults WHERE Spread = 1) END;
+				IF EXISTS (SELECT * FROM tmpResults WHERE Spread = 1 AND WeekNum = CWeekNum - 1) THEN
+					INSERT INTO tmpResults (WeekNum, 
+											DistrictID,
+											TotalPopulation,
+											TotalInfected,
+											Speed,
+											Multiplier,
+											Spread) 
+						SELECT		CWeekNum,
+									C.ToID,
+									E.Employed+E.Unemployed,
+									20,
+									D.StartSpeed,
+									D.Multiplier,
+									0
+						FROM 		Commute C
+									LEFT OUTER JOIN District D ON C.ToID = D.ID
+									LEFT OUTER JOIN Employment E on E.DistrictID = D.ID
+						WHERE		NOT EXISTS (SELECT * FROM tmpResults WHERE DistrictID = D.ID)   
+						ORDER BY 	C.Number DESC
+						LIMIT 		5;
+				END IF;
+     
+				
 				INSERT INTO tmpResults (WeekNum, 
-										DistrictID,
-										DistrictName, 
-                                        Longitude,
-                                        Latitude, 
-									    TotalPopulation,
-									    TotalInfected,
-										Speed,
-                                        Multiplier,
-                                        Spread) 
-					SELECT		DISTINCT CWeekNum,
-								C.ToID,
-								D.Name,
-                                D.Longitude,
-                                D.Latitude,
-								E.Employed+E.Unemployed,
-								20,
-								D.StartSpeed,
-								D.Multiplier,
-								0
-					FROM 		Commute C
-                                LEFT OUTER JOIN District D ON C.ToID = D.ID
-								LEFT OUTER JOIN Employment E on E.DistrictID = D.ID   
-					ORDER BY 	C.Number DESC
-					LIMIT 		5;
-			END IF;
-            
-			INSERT INTO tmpResults (WeekNum, 
-										DistrictID,
-										DistrictName, 
-                                        Longitude,
-                                        Latitude, 
-									    TotalPopulation,
-									    TotalInfected,
-										Speed,
-                                        Multiplier,
-                                        Spread) 
-				SELECT 	CWeekNum, 
-						t.DistrictID,
-						t.DistrictName, 
-                        t.Longitude,
-                        t.Latitude,
-                        t.TotalPopulation,
-						t.TotalInfected,
-						CEILING(D.StartSpeed * t.Multiplier),
-                        (t.Multiplier * 0.989),
-                        CASE 	WHEN (t.TotalPopulation/2) <= t.TotalInfected 
-								THEN 1 
-                                ELSE 0 
-						END
-				FROM 	District D
-						LEFT OUTER JOIN tmpResults t ON t.DistrictID = D.ID
-				WHERE	t.WeekNum = (SELECT MAX(WeekNum))
-                ORDER BY	WeekNum DESC
-                limit SpreadCount;
+											DistrictID,
+											TotalPopulation,
+											TotalInfected,
+											Speed,
+											Multiplier,
+											Spread) 
+					SELECT 	CWeekNum, 
+							t.DistrictID,
+							t.TotalPopulation,
+							t.TotalInfected * (t.Multiplier * 0.989),
+							CEILING(D.StartSpeed * (t.Multiplier * 0.989)),
+							(t.Multiplier * 0.989),
+							CASE ((t.TotalPopulation/4) <= t.TotalInfected) 
+									WHEN TRUE
+										THEN 1 
+									ELSE 0 
+							END
+					FROM 	District D
+							LEFT OUTER JOIN tmpResults t ON t.DistrictID = D.ID
+					WHERE	t.WeekNum = (SELECT MAX(WeekNum))
+                    AND		(
+								SELECT 	t2.DistrictID 
+								FROM 	tmpResults t2 
+								WHERE 	t2.DistrictID = D.ID 
+								AND 	t2.WeekNum = CWeekNum
+                                LIMIT	1
+							) IS NULL
+					ORDER BY	WeekNum DESC
+					LIMIT	 SpreadCount;
                
 
 				-- UPDATE tmpResults Set TotalInfected = -- (SELECT a.TotalInfected * b.Multiplier FROM tmpResults a LEFT OUTER JOIN tmpResults b ON a.DistrictID = b.DistrictID WHERE a.WeekNum = (SELECT MAX(a.WeekNum)) AND b.WeekNum = CWeekNum);
 		END IF;
-		END IF;
 		SET CWeekNum = CWeekNum + 1;
     END WHILE;
 				
-	SELECT	*
-    FROM	tmpResults;
+	SELECT	t.*,
+			D.Name,
+			D.Longitude,
+            D.Latitude
+    FROM	tmpResults t
+    LEFT OUTER JOIN District D on D.ID = t.DistrictID;
 
     DROP TABLE tmpResults;
     
